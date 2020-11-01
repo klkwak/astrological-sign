005175942151daddc30eae95696286db6f032213
kleekie <kyra.kwak@gmail.com>
2020-10-31 22:19:40 -0400

    updated input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("initialValue");
+  const handleInput = (event) => {
+    setInput(event.target.value);
+  };
   return (
     <div className="App">
-     <h1>Hello world!</h1>
+      <h1>Hello world!</h1>
+      <input onChange={handleInput} />
+      <button>Submit</button>
+      <div>{input}</div>
     </div>
   );
 }
